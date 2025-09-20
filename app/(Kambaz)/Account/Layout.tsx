c0e06a03837a1ada147561b3a792cebb63a031b0
bealunala <luna.be@northeastern.edu>
2025-09-19 21:46:12 -0400

    account layout

--- app/(Kambaz)/Account/Layout.tsx
+++ app/(Kambaz)/Account/Layout.tsx
@@ -1,22 +1,55 @@
+// import { ReactNode } from "react";
+// import AccountNavigation from "./Navigation";
+// export default function AccountLayout({
+//   children,
+// }: Readonly<{ children: ReactNode }>) {
+//   return (
+//     <div id="wd-kambaz">
+//       <table>
+//         <tbody>
+//           <tr>
+//             <td valign="top">
+//               <AccountNavigation />
+//             </td>
+//             <td valign="top" width="100%">
+//               {children}
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
 import { ReactNode } from "react";
+import KambazNavigation from "../Navigation";
 import AccountNavigation from "./Navigation";
+
 export default function AccountLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div id="wd-kambaz">
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <AccountNavigation />
-            </td>
-            <td valign="top" width="100%">
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td valign="top" width="200">
+            <KambazNavigation />
+          </td>
+          <td valign="top" width="100%">
+            <table>
+              <tbody>
+                <tr>
+                  <td valign="top">
+                    <AccountNavigation />
+                  </td>
+                  <td valign="top" width="100%">
+                    {children}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
--- app/(Kambaz)/Layout.tsx
+++ app/(Kambaz)/Layout.tsx
@@ -1,46 +1,22 @@
-// import { ReactNode } from "react";
-// import KambazNavigation from "./Navigation";
-
-// export default function KambazLayout({
-//   children,
-// }: Readonly<{ children: ReactNode }>) {
-//   return (
-//     <table>
-//       <tbody>
-//         <tr>
-//           <td valign="top" width="200">
-//             <KambazNavigation />
-//           </td>
-//           <td valign="top" width="100%">
-//             {children}
-//           </td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   );
-// }
-
 import { ReactNode } from "react";
+import KambazNavigation from "./Navigation";
 
 export default function KambazLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <div style={{ border: "2px solid red", padding: "20px" }}>
-      <h1>LAYOUT IS WORKING!</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top" width="200" style={{ border: "1px solid green" }}>
-              <div>NAVIGATION SHOULD BE HERE</div>
-            </td>
-            <td valign="top" width="100%" style={{ border: "1px solid blue" }}>
-              {children}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <td valign="top" width="200">
+            <KambazNavigation />
+          </td>
+          <td valign="top" width="100%">
+            {children}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 

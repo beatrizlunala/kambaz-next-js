e3563d184b3003a0b6e0d537ffd3314a91d3f712
bealunala <luna.be@northeastern.edu>
2025-09-19 21:37:15 -0400

    Update page.tsx

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -1,7 +1,4 @@
 import { redirect } from "next/navigation";
-// export default function Kambaz() {
-//   redirect("/Account/Signin");
-// }
 export default function Kambaz() {
-  return <div>Test - you should see the sidebar now</div>;
+  redirect("/Account/Signin");
 }

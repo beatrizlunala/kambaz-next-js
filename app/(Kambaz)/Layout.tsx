6e5b4abacab0f06740c19a3794abd9ee13a55e7b
bealunala <luna.be@northeastern.edu>
2025-09-19 21:36:04 -0400

    testing deployed site

--- app/(Kambaz)/Layout.tsx
+++ app/(Kambaz)/Layout.tsx
@@ -1,21 +1,34 @@
-import { ReactNode } from "react";
-import KambazNavigation from "./Navigation";
+// import { ReactNode } from "react";
+// import KambazNavigation from "./Navigation";
 
-export default function KambazLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+// export default function KambazLayout({
+//   children,
+// }: Readonly<{ children: ReactNode }>) {
+//   return (
+//     <table>
+//       <tbody>
+//         <tr>
+//           <td valign="top" width="200">
+//             <KambazNavigation />
+//           </td>
+//           <td valign="top" width="100%">
+//             {children}
+//           </td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// }
+
+import { ReactNode } from "react";
+
+export default function KambazLayout({ children }: { children: ReactNode }) {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td valign="top" width="200">
-            <KambazNavigation />
-          </td>
-          <td valign="top" width="100%">
-            {children}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div style={{ border: "2px solid red", padding: "20px" }}>
+      <h1>LAYOUT IS WORKING!</h1>
+      <div style={{ border: "1px solid blue", padding: "10px" }}>
+        {children}
+      </div>
+    </div>
   );
 }

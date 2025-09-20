0ef5e3e231aa5e95745b85947625f1f10d7540d5
bealunala <luna.be@northeastern.edu>
2025-09-19 21:40:38 -0400

    Update Layout.tsx

--- app/(Kambaz)/Layout.tsx
+++ app/(Kambaz)/Layout.tsx
@@ -22,13 +22,37 @@
 
 import { ReactNode } from "react";
 
-export default function KambazLayout({ children }: { children: ReactNode }) {
+export default function KambazLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
     <div style={{ border: "2px solid red", padding: "20px" }}>
       <h1>LAYOUT IS WORKING!</h1>
-      <div style={{ border: "1px solid blue", padding: "10px" }}>
-        {children}
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="200" style={{ border: "1px solid green" }}>
+              <div>NAVIGATION SHOULD BE HERE</div>
+            </td>
+            <td valign="top" width="100%" style={{ border: "1px solid blue" }}>
+              {children}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
+// import { ReactNode } from "react";
+
+// export default function KambazLayout({ children }: { children: ReactNode }) {
+//   return (
+//     <div style={{ border: "2px solid red", padding: "20px" }}>
+//       <h1>LAYOUT IS WORKING!</h1>
+//       <div style={{ border: "1px solid blue", padding: "10px" }}>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// }

bf6861fbaa1e7eee982cede5ce92528b40eca57a
bealunala <luna.be@northeastern.edu>
2025-10-03 13:01:37 -0400

    bug: remove apostrophes

--- app/Labs/Lab2/Positions.tsx
+++ app/Labs/Lab2/Positions.tsx
@@ -39,8 +39,8 @@ export default function Positions() {
       <div id="wd-css-position-fixed">
         <h2>Fixed position</h2>
         Checkout the blue square that says Fixed position stuck all the way on
-        the right and half way down the page. It doesn't scroll with the rest of
-        the page. Its position is Fixed.
+        the right and half way down the page. It does not scroll with the rest
+        of the page. Its position is Fixed.
         <div className="wd-pos-fixed wd-dimension-square wd-bg-color-blue wd-fg-color-white">
           Fixed position
         </div>
